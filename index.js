2c521b0518f0f85fef2870062dbbd651e1fd635d
Robin Chan <robinasaurousrex@gmail.com>
2025-11-21 20:35:55 -0700

    start js file and start logic for contact form

---
+++ index.js
@@ -0,0 +1,14 @@
+const firstName = document.querySelector("#first");
+const lastName = document.querySelector("#last");
+const email = document.querySelector("#email");
+const radioButtons = document.querySelectorAll("#radio1, #radio2");
+const message = document.querySelector("#message");
+const checkbox = document.querySelector("#check");
+const alertBox = document.querySelector(".alert-box");
+const errorMessage = document.querySelectorAll(".error-message");
+
+addEventListener("form submission", (e) => {
+  e.preventDefault;
+});
+// Prevent page from changing when submission of the form is made
+function isValidForm() {}
